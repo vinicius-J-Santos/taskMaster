e5d4a552b27e0808805e2fee0e71f0caa5b4efcb
vinicius Jose <vinicius.j2009@gmail.com>
2026-08-07 23:11:03 -0300

    feat: funcao editar feita

---
+++ acoes/editar.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . "/../bd.php";
+
+if (!isset($_POST["id"])) {
+    header("Location: ../index.php");
+    exit;
+}
+
+$id = (int)$_POST["id"];
+$titulo = $_POST["titulo"];
+$descricao = $_POST["descricao"];
+$horas_estimada = $_POST["horas_estimada"];
+$data_limite = $_POST["data_limite"];
+
+$sql = "update tarefas set titulo = '$titulo', descricao = '$descricao', horas_estimada = '$horas_estimada', data_limite = '$data_limite' where id = $id";
+
+$conn->query($sql);
+
+header("Location: ../index.php");
+exit;
