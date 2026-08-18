3b5fcc711ee1be6d6eb67a89f1c3213eb34c4cec
viniciu <vinicius.j2009@gmail.com>
2026-08-18 09:39:07 -0300

    adicionado banco de dados em txt

--- bd.php
+++ bd.php
@@ -4,7 +4,7 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $dbname = "taskmaster";
-$port = 3306;
+$port = 3308;
 
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
--- tarefas.sql
+++ tarefas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Tempo de geração: 11-Ago-2026 às 12:14
+-- Tempo de geração: 18-Ago-2026 às 12:32
 -- Versão do servidor: 8.4.7
 -- versão do PHP: 8.3.28
 
@@ -30,26 +30,21 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `tarefas`;
 CREATE TABLE IF NOT EXISTS `tarefas` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descricao` text COLLATE utf8mb4_unicode_ci,
+  `titulo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descricao` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `horas_estimada` int NOT NULL,
   `data_limite` date NOT NULL,
   `feito` tinyint(1) NOT NULL,
   `criado_em` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `tarefas`
 --
 
 INSERT INTO `tarefas` (`id`, `titulo`, `descricao`, `horas_estimada`, `data_limite`, `feito`, `criado_em`) VALUES
-(3, 'Refinar o Task Master - PW II', 'Voltar refinando todo o Task Master(Site com PHP de organização de tarefas).\r\nMateira: PW II', 5, '2026-09-15', 0, '2026-08-11 11:41:22'),
-(4, 'Pegar ouro no Brawlhalla', 'Jogar ranqueadas e pegar ouro no jogo Brawlhalla', 1000, '2026-08-30', 0, '2026-08-11 11:42:14'),
-(5, 'Desenhar', 'Fazer alguns desenho no sketch book', 2, '2026-08-13', 1, '2026-08-11 11:42:51'),
-(6, 'Agora essa tarefa esta certa', 'Essa tarefa esta certa, ebaa!', 0, '2026-08-12', 0, '2026-08-11 11:43:40'),
-(7, 'Limpar o tenis', 'Lembrar de limpar o tenis', 0, '2026-08-20', 1, '2026-08-11 11:44:26'),
-(14, 'aaaaa', 'aaaaa', 11, '0001-11-11', 0, '2026-08-11 12:08:47');
+(15, 'Bem Vindo ao Task Master!', 'Isso é um tutorial, fique a vontade para deletar, editar ou completar essa tarefa!', 0, '1111-11-11', 0, '2026-08-18 12:32:03');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
